e33885a8ab6818e5cdb3c91812653ccd0e6fa781
Sam Hoover <samuel.charles.hoover@gmail.com>
2025-07-11 11:47:08 -0400

    Sync

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,6 +15,7 @@ return {
     },
     picker = { enabled = true },
     quickfile = { enabled = true },
+    rename = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
